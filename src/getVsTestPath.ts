ed1fe18c0dce842e05870e338cc3fd2283355219
Kirill Larionov <kirill.larionov@visma.com>
2024-08-14 13:17:41 +0200

    VMM-5758 Fixing paths

--- src/getVsTestPath.ts
+++ src/getVsTestPath.ts
@@ -13,13 +13,6 @@ export function getVsTestPath(): string {
   }
 
   let vsTestVersion = core.getInput('vsTestVersion')
-  if(vsTestVersion && vsTestVersion === "14.0") {
-    return path.join(__dirname, 'win-x64/VsTest/v140/vstest.console.exe')
-  }
 
-  if(vsTestVersion && vsTestVersion === "15.0") {
-    return path.join(__dirname, 'win-x64/VsTest/v150/Common7/IDE/Extensions/TestPlatform/vstest.console.exe')
-  }
-
-  return path.join(__dirname, 'win-x64/VsTest/v160/Common7/IDE/Extensions/TestPlatform/vstest.console.exe')
+  return path.join(__dirname, 'TestPlatform/vstest.console.exe')
 }
